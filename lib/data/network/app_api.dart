2dd019dd93659eca36ff599d003ee9338fb542a4
hirammaina <55752706+tabbykaruga@users.noreply.github.com>
2023-05-18 11:15:53 +0300

    Revert "Revert "JSON""
    This reverts commit b05ea91a861d937a75a3b44f235687e0027807ea.

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import '../../app/constants.dart';
+import '../responses/responses.dart';
+part 'app_api.g.dart';
+
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceUser {
+  factory AppServiceUser(Dio dio,{String baseUrl}) =_AppServiceUser;
+  
+  @POST("login")
+  Future<AuthenticationResponse> login();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app/myApp.dart';
+import 'app/app.dart';
 
 void main() {
   runApp(MyApp());
